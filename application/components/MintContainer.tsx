c7c02949338f5f5263c3f7e2e5f599573d970e24
Soolking-cyber <54062088+Soolking-cyber@users.noreply.github.com>
2024-07-02 21:34:29 +0700

    Update MintContainer.tsx

--- application/components/MintContainer.tsx
+++ application/components/MintContainer.tsx
@@ -6,18 +6,18 @@ import styles from "../styles/Home.module.css";
 export default function MintContainer() {
   return (
     <div className={styles.collectionContainer}>
-      <h1>Frog Wars - BURN BABY, BURN!</h1>
-      <p>MINT FROG WARS COMIC BOOK EDITION 2 NFT to start playing!</p>
+      <h1>Frog Wars - HUNGER FOR POWER!</h1>
+      <p>MINT FROG WARS COMIC BOOK EDITION 1 NFT to start playing!</p>
 
       <div className={`${styles.nftBox} ${styles.spacerBottom}`}>
-        <Image src="/book.jpg" width={"120"} height={"200"} alt="mine" />
+        <Image src="/book.jpg" width={"240"} height={"400"} alt="mine" />
       </div>
 
       <div className={styles.smallMargin}>
         <Web3Button
           theme="dark"
           contractAddress={CHARACTER_EDITION_ADDRESS}
-          action={(contract) => contract.erc1155.claim(2, 1)}
+          action={(contract) => contract.erc1155.claim(0, 1)}
         >
           MINT 
         </Web3Button>
